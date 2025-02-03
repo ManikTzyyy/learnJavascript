9ab25767166be3a1c13845ea471af82cb3234a02
ManikTzyyy <manikyogantara1@gmail.com>
2025-02-03 11:20:44 +0800

    method push and pop array js

--- array.js
+++ array.js
@@ -10,3 +10,22 @@ console.log(warna);
 warna[1] = "test";
 console.log(warna);
 
+// method push and pop
+console.log("array method push and pop")
+let number =[];
+console.log(number)
+console.log("method push")
+number.push(0)
+number.push(2)
+number.push(5)
+number.push(7)
+number.push(9)
+
+console.log(number)
+
+console.log("method pop")
+number.pop()
+console.log(number)
+
+
+

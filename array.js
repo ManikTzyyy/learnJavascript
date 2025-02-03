d1b1b38c00b82e3cf17f261f98f666a4495a70e7
ManikTzyyy <manikyogantara1@gmail.com>
2025-02-03 11:12:45 +0800

    basic array

--- array.js
+++ array.js
@@ -1,5 +1,11 @@
-let angka = [1,2,3,4,5];
+let angka = [1, 2, 3, 4, 5];
 
-console.log(angka[2])
+console.log(angka[2]);
 
 console.log(angka);
+
+let warna = ["merah", "hijau", "biru"];
+console.log(warna);
+
+warna[1] = "test";
+console.log(warna);

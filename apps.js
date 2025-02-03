3680d07cdbc7f5a0a6244593db2abfef5e593795
ManikTzyyy <manikyogantara1@gmail.com>
2025-02-03 10:24:22 +0800

    looping using while and fot

--- apps.js
+++ apps.js
@@ -1,8 +1,22 @@
 const kkm = 80;
 let nilai = 90;
 
-if (nilai > kkm ) {
-    console.log("selamat kamu lulus");
-}else {
-    console.log("kamu tidak lulus")
+if (nilai > kkm) {
+  console.log("selamat kamu lulus");
+  //   alert("Lulus");
+} else {
+  console.log("kamu tidak lulus");
+}
+
+// looping using while and for
+
+let i = 0;
+
+while (i < 10) {
+  console.log("this using while" + " " + i);
+  i++;
+}
+
+for (i = i; i < 20; i++) {
+  console.log("this using for" + " " + i);
 }
